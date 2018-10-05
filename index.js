60cfa21f568a685ddedd3a8a21a3c9e94864a207
Helen Lawes <helen.lawes@mirumagency.com>
2018-10-05 15:37:26 +0100

    adding in option for changing directory to create component

--- index.js
+++ index.js
@@ -1,23 +1,34 @@
 #!/usr/bin/env node
 
 const args = require('args'),
-	fs = require('fs-extra');
+	fs = require('fs-extra'),
+	path = require('path');
 
 const currentDir = process.cwd();
 
-const createFiles = (name) => {
-	fs.copy(`${__dirname}/templates/Component.js`, `${currentDir}/components/${name}/${name}.js`);
-	fs.copy(`${__dirname}/templates/Component.story.js`, `${currentDir}/components/${name}/${name}.story.js`);
-	fs.copy(`${__dirname}/templates/Component.styles.js`, `${currentDir}/components/${name}/${name}.styles.js`);
+const createFiles = (name, dir) => {
+	const componentFile = path.resolve(currentDir, dir, `${name}/${name}.js`);
+	const storyFile = path.resolve(currentDir, dir, `${name}/${name}.story.js`);
+	const styleFile = path.resolve(currentDir, dir, `${name}/${name}.styles.js`);
+	fs.copy(`${__dirname}/templates/Component.js`, componentFile);
+	fs.copy(`${__dirname}/templates/Component.story.js`, storyFile);
+	fs.copy(`${__dirname}/templates/Component.styles.js`, styleFile);
+	/* eslint-disable no-console */
+	console.log('created component files:');
+	console.log(`* ${componentFile}`);
+	console.log(`* ${storyFile}`);
+	console.log(`* ${styleFile}`);
+	/* eslint-enable no-console */
 };
 
-const component = (name, sub) => {
-	createFiles(sub[0]);
+const component = (name, sub, options) => {
+	createFiles(sub[0], options.dir);
 };
 
 args.config.name = 'rg';
 
 args
+	.option('dir', 'Directory to create component', 'src/components')
 	.command('component', 'Create a new react component', component, ['c']);
 
 
