73a85ce129cd9240208652218168be9f9583dfe7
Helen Lawes <helen.lawes@mirumagency.com>
2018-10-10 15:28:46 +0100

    adding in ability to turn off file being created

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const openFileReplace = async (path, replacement) => {
 	return fileContents.replace(/Component/g, replacement);
 };
 
-const createFiles = async (name, dir) => {
+const createFiles = async (name, dir, styles, story) => {
 	const componentFile = path.resolve(currentDir, dir, `${name}/${name}.js`);
 	const storyFile = path.resolve(currentDir, dir, `${name}/${name}.story.js`);
 	const styleFile = path.resolve(currentDir, dir, `${name}/${name}.styles.js`);
@@ -19,28 +19,36 @@ const createFiles = async (name, dir) => {
 	let cfContents = await openFileReplace(`${__dirname}/templates/Component.js`, name);
 	fs.outputFileSync(componentFile, cfContents);
 
-	let storyContents = await openFileReplace(`${__dirname}/templates/Component.story.js`, name);
-	fs.outputFileSync(storyFile, storyContents);
+	if (story) {
+		let storyContents = await openFileReplace(`${__dirname}/templates/Component.story.js`, name);
+		fs.outputFileSync(storyFile, storyContents);
+	}
 
-	let styleContents = await openFileReplace(`${__dirname}/templates/Component.styles.js`, name);
-	fs.outputFileSync(styleFile, styleContents);
+	if (styles) {
+		let styleContents = await openFileReplace(`${__dirname}/templates/Component.styles.js`, name);
+		fs.outputFileSync(styleFile, styleContents);
+	}
 
 	/* eslint-disable no-console */
 	console.log('created component files:');
 	console.log(`* ${componentFile}`);
-	console.log(`* ${storyFile}`);
-	console.log(`* ${styleFile}`);
+	story && console.log(`* ${storyFile}`);
+	styles && console.log(`* ${styleFile}`);
 	/* eslint-enable no-console */
 };
 
 const component = (name, sub, options) => {
-	createFiles(sub[0], options.dir);
+	createFiles(sub[0], options.dir, parseBool(options.styles), parseBool(options.story));
 };
+
+const parseBool = value => value === 'true' ? true : false;
 
 args.config.name = 'rg';
 
 args
 	.option('dir', 'Directory to create component', 'src/components')
+	.option('styles', 'Enable styles file to be created for component', 'true')
+	.option('story', 'Enable story file to be created for component', 'true')
 	.command('component', 'Create a new react component', component, ['c']);
 
 
